ef0164f405f479684f8f7227528ad87ea900f8f6
Andrew Tai <andrewtai.ye@gmail.com>
2022-07-01 22:19:40 +0800

    added addcolor button

--- App.js
+++ App.js
@@ -1,17 +1,32 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
-import { StyleSheet, Text, View, FlatList } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  Button,
+  TouchableOpacity,
+} from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
 
 import BlockRGB from "./components/BlockRGB";
 
 function HomeScreen() {
-  const [colorArray, setColorArray] = useState([
-    { red: 255, green: 0, blue: 0, id: "0" },
-    { red: 0, green: 255, blue: 0, id: "1" },
-    { red: 0, green: 0, blue: 255, id: "2" },
-  ]);
+  function addColor() {
+    setColorArray([
+      ...colorArray,
+      {
+        red: Math.floor(Math.random() * 256),
+        green: Math.floor(Math.random() * 256),
+        blue: Math.floor(Math.random() * 256),
+        id: `${colorArray.length}`,
+      },
+    ]);
+  }
+
+  const [colorArray, setColorArray] = useState([]);
 
   function renderItem({ item }) {
     return <BlockRGB red={item.red} green={item.green} blue={item.blue} />;
@@ -19,6 +34,9 @@ function HomeScreen() {
 
   return (
     <View style={styles.container}>
+      <TouchableOpacity style={styles.button} onPress={addColor}>
+        <Text style={{ color: "red" }}>Add Colour</Text>
+      </TouchableOpacity>
       <FlatList style={styles.list} data={colorArray} renderItem={renderItem} />
     </View>
   );
@@ -46,5 +64,9 @@ const styles = StyleSheet.create({
   list: {
     padding: 10,
     width: "100%",
+  },
+  button: {
+    height: 40,
+    justifyContent: "center",
   },
 });
